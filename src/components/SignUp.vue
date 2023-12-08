12b311b8d496312db695c97860b20ce870fd86b7
rex <rextambua1@gmail.com>
2023-12-08 16:53:29 +0300

    .

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -17,36 +17,32 @@ export default {
     name: 'SignUp',
 
     data() {
-        return{
-            name:'',
-            email:'',
-            password:''
+        return {
+            name: '',
+            email: '',
+            password: ''
         }
     },
-    methods:{
-        async signUp(){
-            let result = await axios.post("http://localhost:3000/users",{
-            name:this.name,
-            email:this.email,
-            password:this.password,
+    methods: {
+        async signUp() {
+            let result = await axios.post("http://localhost:3000/users", {
+                name: this.name,
+                email: this.email,
+                password: this.password,
             });
 
             console.log(result);
-            if(result.status == 201)
-            {
+            if (result.status == 201) {
                 localStorage.setItem("user-infos", JSON.stringify(result.data))
-                this.$router.push({name:'Home-page'})
+                this.$router.push({ name: 'Home-page' })
             }
         }
     },
-    mounted(){
-            let user = localStorage.getItem('user-infos');
-            if(user)
-            {this.$router.push({name:'Home-page'})}
+    mounted() {
+        let user = localStorage.getItem('user-infos');
+        if (user) { this.$router.push({ name: 'Home-page' }) }
     },
 }
 </script>
 
-<style >
-
-</style>
+<style ></style>
